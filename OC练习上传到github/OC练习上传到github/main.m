8d18007fb11d0bd19163fd99c10ae5713bbe34f4
泽熙 <zhao95@outlook.com>
2016-05-25 16:16:58 +0800

    OC的题
    主要为了练习上传到github

---
+++ OC练习上传到github/OC练习上传到github/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OC练习上传到github
+//
+//  Created by 赵恩峰 on 16/5/25.
+//  Copyright © 2016年 赵恩峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"练习上传到github");
+        NSLog(@"练习上传到github");
+        NSLog(@"练习上传到github");
+    }
+    return 0;
+}
